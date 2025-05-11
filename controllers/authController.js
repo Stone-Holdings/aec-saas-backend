79628651ee4fd1c59101c06868ed5d5988113891
Stone-Holdings <120699543+Stone-Holdings@users.noreply.github.com>
2025-05-11 19:51:15 -0400

    Create authController.js

---
+++ controllers/authController.js
@@ -0,0 +1,11 @@
+// Placeholder function for login
+const login = (req, res) => {
+    res.send('Login Controller');
+};
+
+// Placeholder function for signup
+const signup = (req, res) => {
+    res.send('Signup Controller');
+};
+
+module.exports = { login, signup };
